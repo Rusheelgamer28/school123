3c8978e3d6e6ce96aed5bd27618bc8c17e307ddc
Rusheelgamer28 <86188966+Rusheelgamer28@users.noreply.github.com>
2022-12-24 15:56:17 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,8 @@
+function addUser()
+{
+   var user = document.getElementById("User").value 
+   var pw = document.getElementById("PW").value
+
+   localStorage.setItem("pw", pw)
+   localStorage.setItem("user", user)
+}
